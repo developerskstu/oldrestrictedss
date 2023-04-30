394d8e13f2141693aff8ba5469d6e7a0cb0c3ea6
developerssh <119683709+developerssh@users.noreply.github.com>
2023-04-30 13:45:26 +0530

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -12,12 +12,12 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
                     level=logging.WARNING)
 
 # variables
-API_ID = 25733211
-API_HASH = "e0d2f6d53490d4a6ce5fecce2dd8e25b"
-BOT_TOKEN = "6068536413:AAHGo7Y_17NSLXN54NfxwkR3gJ_4ahTuVtg"
-SESSION = "AQCpKcQZeDtiezPUack9pnx8sHJCEQqm1vC6x9ZbyJ2tohRGz4qnXayvt0j06XvIoitYet50lVgdZrdRO-86nFzs2YJUkcIto0xD30CUYMOXh8EdPG2VotOLGDpIsHsfEmL1zvk1bePaYVuA7UQAoBwE-HPDNzY2g0tg6TpKXp4R_1oqAZNoTm_7eDtOzM_zUqeX4l-lMBd6Pzfs-x9AJPfw6H53zX07fUKh5W4VMbfw9fY4grk7xa6Xekg0VtJCBfgNrI65hJjr71Yal6D7QhWPyc-dVTYzSrrwfHPQTnTg_sQtgIwHWuIRlGDpg2_Yu_sBnhNqg4AzFrwdOj8UugrqAAAAAUxAXxEA"
-FORCESUB = "ttest_botsss"
-AUTH = 5574254353
+API_ID = 18105029
+API_HASH = "f3143f24c70415ca357cc9fd27a1d72f"
+BOT_TOKEN = "5841270397:AAHm9zCnp58lAipCEXMQIttOvTg7-siKfaM"
+SESSION = "AQBIkCKyPUdMxoL_6Z8I4LurXsUBVECxc9f4fpjOGnVC95gwY7enrWd8i35eRII7Gia_1-W3di8YN8p0bVwmSWq6wAxIaagNgQvwBlygbMCDB03kpQhF4szN1VYE3be0PAgBNug1vQvDWzgdw7NBlMMbTkxtTT4gx2o8UfGlFLpzVlBFqqD2dxUyb8eZ7VvD1XLRJB747WYEgCi8kD0M0tVPBfmzasQCyWuVFG-T6WL5GNwGizMtMz7FOfAz1kGWnKJG03VKa-IM913DKHRdecY4Yw-WyghnKKjrtANu5W0q02Oq23-kNXAAv5JqDXrDKP8HLqshj9iGVzb3PoztxuNnAAAAATWA_dAA"
+FORCESUB = "testedd_botss"
+AUTH = 5192613328
 
 bot = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN) 
 
